0e42026e699ad114c2a313dbab326da2b544ee29
Stijn Oomes <stijn.oomes@gmail.com>
2016-01-22 10:01:08 +0100

    Initial Commit

---
+++ DetectFaces/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DetectFaces
+//
+//  Created by Stijn Oomes on 22/01/16.
+//  Copyright © 2016 Oomes Vision Systems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
